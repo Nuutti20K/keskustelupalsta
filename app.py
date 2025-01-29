86bea3589800c23155b77dad032b0ea56e46bd91
Nuutti Kekkonen <nuutti.kekkonen@saunalahti.fi>
2025-01-29 17:43:12 +0200

    Added user registering

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+import sqlite3
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
+import users
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat!"
+    try:
+        users.create_user(username, password1)
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+    
+    return redirect("/")
